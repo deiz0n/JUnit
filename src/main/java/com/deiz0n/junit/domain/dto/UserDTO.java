00901f2b094ad3380a31bbef414418f720e5f8c2
deiz0n <eduardomacedosilva@outlook.com>
2023-12-29 11:42:21 -0300

    Criação do UserRepository e UserDTO

--- src/main/java/com/deiz0n/junit/domain/dto/UserDTO.java
+++ src/main/java/com/deiz0n/junit/domain/dto/UserDTO.java
@@ -1,2 +1,21 @@
-package com.deiz0n.junit.domain.dto;public class UserDTO {
+package com.deiz0n.junit.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String senha;
+
 }
--- src/main/java/com/deiz0n/junit/repositories/UserRepositpory.java
+++ src/main/java/com/deiz0n/junit/repositories/UserRepositpory.java
@@ -1,2 +1,7 @@
-package com.deiz0n.junit.repositories;public interface UserRepositpory {
+package com.deiz0n.junit.repositories;
+
+import com.deiz0n.junit.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepositpory extends JpaRepository<User, Long> {
 }
